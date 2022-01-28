7a61c9c842ca0a602339cbc2e1ff4da2040e4814
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2022-01-28 16:00:31 +0800

    Add files via upload

---
+++ spring-boot-jdbctemplate-crud-example2/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `tutorials` (`id`, `title`, `description`, `published`) VALUES (1,'Adobe After Effect Tutorial#1','Adobe After Effect Tutorial#1',0);
+INSERT INTO `tutorials` (`id`, `title`, `description`, `published`) VALUES (2,'Adobe Premiere Tutorial#2','Adobe Premiere Tutorial#2',0);
+INSERT INTO `tutorials` (`id`, `title`, `description`, `published`) VALUES (3,'Android Tutorial#3','Android Tutorial#3',0);
+INSERT INTO `tutorials` (`id`, `title`, `description`, `published`) VALUES (4,'Ansible Tutorial#4','Ansible Tutorial#4',0);
+INSERT INTO `tutorials` (`id`, `title`, `description`, `published`) VALUES (5,'Apache Ant Tutorial#4','Apache Ant Tutorial#4',0);
